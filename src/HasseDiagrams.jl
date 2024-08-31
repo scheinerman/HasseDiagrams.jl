2e217c59f7b17ffa119646a33ae129df8d343d7f
Ed Scheinerman <ers@jhu.edu>
2024-08-31 13:11:16 -0400

    Primordial

--- src/HasseDiagrams.jl
+++ src/HasseDiagrams.jl
@@ -1,5 +1,33 @@
 module HasseDiagrams
 
-greet() = print("Hello World!")
+using Posets
+using SimpleDrawing
+
+export HasseDiagram
+
+struct HasseDiagram
+    p::Poset
+    xy::Dict{Int,Vector{Float64}}
+    radius::Real
+    fill_color::Symbol
+
+    labels::Dict{Int,Any}
+    label_size::Int
+
+    function HasseDiagram(p::Poset)
+        n = nv(p)
+        xy = Dict{Int,Vector{Float64}}()
+        radius = 12
+        fill_color = :white
+
+        labels = Dict{Int,Any}()
+        for v in 1:n
+            labels[v] = v
+        end
+        label_size = 0
+
+        return new(p, xy, radius, fill_color, labels, label_size)
+    end
+end
 
 end # module HasseDiagrams
