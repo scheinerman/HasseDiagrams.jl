cd64d12b081d1e9691d8f844e22569ff8ac3c35d
Ed Scheinerman <ers@jhu.edu>
2024-09-28 16:13:01 -0400

    Docs update with logo

---
+++ extras/logo.jl
@@ -0,0 +1,15 @@
+using Posets, HasseDiagrams, Plots, SimpleDrawing
+
+function hasse_logo()
+    p = chain(1) / (chain(2) + chain(1)) / chain(1)
+    p = p'
+    h = HasseDiagram(p)
+    set_xy!(h, force_layout)
+    scalex!(h, 1.5)
+    set_radius!(h,32)
+    set_font_size!(h,30)
+    
+    draw(h)
+    expand_canvas()
+    finish()
+end
